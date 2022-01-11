de97702aabf28f86e55d11a87481c03f8380c91d
rinel <rineliniguezsosa@gmail.com>
2022-01-11 14:04:14 -0600

    creando mi servidor express
    inicializando express e inicializando el puerto de escucha

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 const express = require('express')
+const app = express()
+const port = 3000;
 
+
+
+
+
+app.listen(port,()=>{
+    console.log(`El servicio esta corriendo http://localhost:${port}`)
+})

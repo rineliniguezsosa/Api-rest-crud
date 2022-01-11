f406854f66129f7ad4051e2ab17337383b9c035c
rinel <rineliniguezsosa@gmail.com>
2022-01-11 14:38:24 -0600

    definiedo el squema
    definiedo el squema mediante mongoose y exportando el squema

--- models/Usuarios.js
+++ models/Usuarios.js
@@ -1,1 +1,11 @@
 const mongoose = require('mongoose')
+
+
+const usuario = mongoose.Schema({
+    nombre:{type:String,required:true},
+    edad:{type:Number,required:true},
+    email:{type:String,required:true}
+})
+
+
+module.exports = usuario

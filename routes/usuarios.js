800386e0d132c0d5aeda73ff5c774a1b2bf9aed5
rinel <rineliniguezsosa@gmail.com>
2022-01-11 17:16:33 -0600

    obteniendo todos los usuarios

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -3,7 +3,13 @@ const router = express.Router()
 const Modelusuario = require('../models/Usuarios')
 
 router.get('/todos',async(req,res)=>{
-    
+    const todos = await Modelusuario.find({})
+    try{
+        res.send(todos)
+    }catch(err){
+        res.send("Message:Algo salio mal")
+    }
+
 })
 
 router.post('/agregar',async(req,res)=>{

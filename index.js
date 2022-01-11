0414330a6a258d3c3cd01a5cd6efebc2f7284c97
rinel <rineliniguezsosa@gmail.com>
2022-01-11 14:51:15 -0600

    definiendo nuestra conexión con mongoose

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const router = require('./routes/usuarios')
-const mongose = require('mongose')
+const mongoose = require('mongoose')
 const app = express()
 const port = 3000;
 
 app.use('/crud',router)
 
-
-
-
 app.listen(port,()=>{
     console.log(`El servicio esta corriendo http://localhost:${port}`)
+})
+
+mongoose.connect("mongodb://localhost:27017",(err)=>{
+    err ? console.log("Algo salio mal en la conexion !!") : console.log("Conexión exitosa !!")
 })

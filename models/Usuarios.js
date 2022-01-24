2487b25bdc7ad8b7364475ba1d5a7306fd1dcbb7
rinel <rineliniguezsosa@gmail.com>
2022-01-24 14:29:01 -0600

    uniendo cambios a develop

--- models/Usuarios.js
+++ models/Usuarios.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 
 
 const Usuario = mongoose.Schema({
-    nombre:{type:String,required:true},
-    edad:{type:Number,required:true},
+    nombre:{type:String,required:true,min:[10,'minimo 10 palabras']},
+    edad:{type:Number,required:true,max:[50,'Solo 50 años es el rango de edad establecido']},
     email:{type:String,required:true}
 })
 

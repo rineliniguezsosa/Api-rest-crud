ae22e9109139441a9c295863548bd7475f732715
rinel <rineliniguezsosa@gmail.com>
2022-01-24 16:31:55 -0600

    comentando atributos del sistema

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const usandolasrutas = require('./routes/usuarios')
-require('dotenv').config()
+// require('dotenv').config()
 
 const app = express()
 app.use(express.json())
 app.use('/crud',usandolasrutas)
 
-app.listen(process.env.PORT,()=>{
+app.listen("3000",()=>{
     console.log(`El servicio esta corriendo http://localhost:3000`)
 })
 
-mongoose.connect(process.env.MONGOCONNECT,(err)=>{
+mongoose.connect("mongodb://localhost:27017",(err)=>{
     err ? console.log("Algo salio mal en la conexion !!") : console.log("Conexión exitosa !!")
 })

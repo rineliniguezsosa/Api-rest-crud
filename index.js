02ee6e6f33371d6edaae9d1c3088f0e1fc17eaa2
rinel <rineliniguezsosa@gmail.com>
2022-01-24 15:45:10 -0600

    importando dotenv

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const usandolasrutas = require('./routes/usuarios')
-
+require('dotenv').config()
 
 const app = express()
 app.use(express.json())

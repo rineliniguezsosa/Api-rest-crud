a4c1ebf943f3e137661b939e8e2f0cd2a4fcc31c
rinel <rineliniguezsosa@gmail.com>
2022-01-24 21:46:38 -0600

    moviendo el contenido get a todoslosusuarios

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -2,5 +2,10 @@ const Modelusuario = require('../models/Usuarios')
 
 
 const todoslosusuarios = async ()=>{
-    
+    const todos = await Modelusuario.find({})
+    try{
+        res.send(todos)
+    }catch(err){
+        res.send("Message:Algo salio mal")
+    }
 }

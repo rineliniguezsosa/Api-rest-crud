fa52be4ade7d7b7b343021faaa563ea609740070
rinel <rineliniguezsosa@gmail.com>
2022-01-24 23:54:57 -0600

    añadiendo la referencia actualizabyid

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -17,15 +17,6 @@ router.delete('/deletebyid/:id',deletebyid)
 
 router.post('/agregar',agregarusuario)
 
-router.put('/actualizabyid/:id',async(req,res)=>{
-    const id = req.params.id
-    //{$set:{nombre:req.body,edad:req.body,email:req.body}}
-    const actualiza = await Modelusuario.updateOne({_id:id},{$set:req.body})
-    try{
-        res.send(actualiza)
-   }catch(err){
-      res.send("Message:Algo salio mal")
-   }
-})
+router.put('/actualizabyid/:id',actualizabyid)
 
 module.exports = router

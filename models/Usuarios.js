b782e3ac0746fcd5d93ccc190e5d1c7e569c11b0
rinel <rineliniguezsosa@gmail.com>
2022-01-25 15:20:58 -0600

    eliminando datos del squema en campos edad y email

--- models/Usuarios.js
+++ models/Usuarios.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose')
 
 const Usuario = mongoose.Schema({
     nombre:{type:String},
-    edad:{type:Number,required:true,max:[50,'Solo 50 años es el rango de edad establecido']},
-    email:{type:String,required:true}
+    edad:{type:Number},
+    email:{type:String}
 })
 
 

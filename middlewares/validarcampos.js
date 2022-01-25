c0d13c638f8f322e70d760007ac54268ba259d0a
rinel <rineliniguezsosa@gmail.com>
2022-01-25 13:44:40 -0600

    estableciendo respuesta de error

--- middlewares/validarcampos.js
+++ middlewares/validarcampos.js
@@ -1,8 +1,14 @@
 const { validationResult } = require('express-validator')
 
 
-const validarcampos = () =>{
+const validarcampos = (req,res,next) =>{
+    const errors = validationResult(req)
 
+    if( !errors.isEmpty() ){
+        return res.status(400).json(errors);
+    }
+
+    next();
 }
 
 module.exports = validarcampos
